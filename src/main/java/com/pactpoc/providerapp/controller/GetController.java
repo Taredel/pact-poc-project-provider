f5e496847d2a743d97eab20c42057bf2b063d79f
Taredel <taredel@mail.ru>
2023-08-10 10:39:40 +0400

    contract changed

--- src/main/java/com/pactpoc/providerapp/controller/GetController.java
+++ src/main/java/com/pactpoc/providerapp/controller/GetController.java
@@ -14,13 +14,13 @@ public class GetController {
     @RequestMapping(value = "/getSomething",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> printData(@RequestParam int inputData, @RequestParam String message, @RequestHeader String authToken) {
+    public ResponseEntity<?> printData(@RequestParam int inputData, @RequestHeader String authToken) {
         System.out.println("Retrieved data - " + inputData);
         System.out.println("Retrieved token - " + authToken);
         if (inputData == 0) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(new SimpleDto(inputData, new MessageDto(message)), HttpStatus.OK);
+        return new ResponseEntity<>(new SimpleDto(inputData, new MessageDto("Default message")), HttpStatus.OK);
     }
 
 }
