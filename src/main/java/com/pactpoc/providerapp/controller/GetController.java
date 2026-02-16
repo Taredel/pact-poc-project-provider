2851ae89844a64736dcd60fd37671e906bd3a193
Taredel <taredel@mail.ru>
2026-02-16 12:31:29 +0100

    ngrok version

--- src/main/java/com/pactpoc/providerapp/controller/GetController.java
+++ src/main/java/com/pactpoc/providerapp/controller/GetController.java
@@ -17,9 +17,9 @@ public class GetController {
     public ResponseEntity<?> printData(@RequestParam int inputData, @RequestHeader String authToken) {
         System.out.println("Retrieved data - " + inputData);
         System.out.println("Retrieved token - " + authToken);
-        if (inputData == 0) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+//        if (inputData == 0) {
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
         return new ResponseEntity<>(new SimpleDto(inputData, new MessageDto("This is a message")), HttpStatus.OK);
     }
 
